9eb07a65198b8a8f121888e334b4dee3baaa9ebb
benschi11 <benedikt.neuhold@live.at>
2014-09-20 12:15:31 -0500

    Added two modified tiny-mce init files (for textarea and textfield). Modify the Parts.Title.InlineEdit.cshtml to use the tiny-mce for inline-editing.

---
+++ Scripts/orchard-inlineedit-textarea.js
@@ -0,0 +1,94 @@
+﻿(function ($, window, inlineedit, tinymce) {
+    "use strict";
+
+    var $inlineedit = $(inlineedit);
+
+    $inlineedit.bind(inlineedit.events.retrieveContent, function (event, scope, shapeEditor) {
+        console.log('Processing Content: TinyMce Editor');
+
+        if (tinymce != undefined) {
+            console.log('Processing Content: TinyMce Editor triggering save');
+            var editorFieldId = $(shapeEditor).find(".tinymce").attr("id");
+            var activeEditor = tinymce.get(editorFieldId);
+            if (activeEditor.isDirty()) {
+                var afterContent = activeEditor.save();
+                $('#' + activeEditor.id).parent('form').children('textarea').val(afterContent);
+            }
+
+            console.log('Processing Content: TinyMce Editor triggering saved');
+        }
+
+        console.log('Processed Content: TinyMce Editor');
+    });
+
+    $inlineedit.bind(inlineedit.events.editorPrepared, function (event, shape, shapeEditor) {
+        console.log('Finalizing Editor');
+
+        var mediaPlugins = ',|';
+
+
+        if (mediaLibraryEnabled) {
+            mediaPlugins += ",medialibrary";
+        }
+
+        //if (shapeEditor.MetadataType == 'Parts_Common_Body') {
+            var selector = '#' + shape.id + ' div.tinymce';
+
+            var editorShapeId = $(selector).attr('id');
+            var newEditorShapeId = shape.id + '_' + editorShapeId;
+            $(selector).attr('id', newEditorShapeId);
+
+            tinymce.init({
+                selector: '#' + newEditorShapeId,
+                theme: 'modern',
+                schema: 'html5',
+                plugins: 'fullscreen,autoresize,searchreplace,link,charmap,code' + mediaPlugins.substr(2),
+                toolbar: 'searchreplace,|,cut,copy,paste,|,undo,redo' + mediaPlugins + ',|,link,unlink,charmap,|,bold,italic,|,numlist,bullist,formatselect,|,code,fullscreen,',
+                convert_urls: false,
+                valid_elements: '*[*]',
+                // shouldn't be needed due to the valid_elements setting, but TinyMCE would strip script.src without it.
+                extended_valid_elements: 'script[type|defer|src|language]',
+                menubar: false,
+                statusbar: false,
+                skin: 'orchardlightgray',
+                inline: true
+            });
+        //}
+
+        console.log('Finalized Editor');
+    });
+
+
+    $inlineedit.bind(inlineedit.events.onCancelling, function (event, shape, shapeEditor) {
+        removeEditor(shape, shapeEditor);
+
+    });
+
+    $inlineedit.bind(inlineedit.events.onSaved, function (event, shape, shapeEditor) {
+        removeEditor(shape, shapeEditor);
+    });
+
+    var removeEditor = function(shape, shapeEditor) {
+        var editorFieldId = $(shapeEditor).find(".tinymce").attr("id");
+        console.log("Try to remove TinyMce from Field with ID: " + editorFieldId);
+        tinymce.get(editorFieldId).remove();
+    };
+
+    var editor = {
+
+    };
+
+    if (!window.orchard) {
+        window.orchard = {};
+    }
+
+    if (!window.orchard.inlineedit) {
+        window.orchard.inlineedit = {};
+    }
+
+    if (!window.orchard.inlineedit) {
+        window.orchard.inlineedit.ui = {};
+    }
+
+    window.orchard.inlineedit.defaulteditor = editor;
+})(jQuery, window, window.orchard.inlineedit.ui, window.tinymce);
---
+++ Scripts/orchard-inlineedit-textfield.js
@@ -0,0 +1,84 @@
+﻿(function ($, window, inlineedit, tinymce) {
+    "use strict";
+
+    var $inlineedit = $(inlineedit);
+
+    $inlineedit.bind(inlineedit.events.retrieveContent, function (event, scope, shapeEditor) {
+        console.log('Processing Content: TinyMce Editor');
+
+        if (tinymce != undefined) {
+            console.log('Processing Content: TinyMce Editor triggering save');
+
+            var editorFieldId = $(shapeEditor).find(".tinymce").attr("id");
+            var activeEditor = tinymce.get(editorFieldId);
+            if (activeEditor.isDirty()) {
+                var afterContent = activeEditor.getContent({ format: 'text' });
+                var fieldId = $('#' + activeEditor.id).attr("data-fieldId");
+
+                $('#' + fieldId).val(afterContent);
+            }
+
+            console.log('Processing Content: TinyMce Editor triggering saved');
+        }
+
+        console.log('Processed Content: TinyMce Editor');
+    });
+
+    $inlineedit.bind(inlineedit.events.editorPrepared, function (event, shape, shapeEditor) {
+        console.log('Finalizing Editor');
+
+            var selector = '#' + shape.id + ' div.tinymce';
+
+            var editorShapeId = $(selector).attr('id');
+            var newEditorShapeId = shape.id + '_' + editorShapeId;
+            $(selector).attr('id', newEditorShapeId);
+
+            console.log(newEditorShapeId);
+
+
+            tinymce.init({
+                selector: '#' + newEditorShapeId,
+                theme: 'modern',
+                schema: 'html5',
+                plugins: "paste",
+                toolbar: 'undo redo',
+                convert_urls: false,
+                entity_encoding: "raw",
+                valid_elements: '*[*]',
+                // shouldn't be needed due to the valid_elements setting, but TinyMCE would strip script.src without it.
+                extended_valid_elements: 'script[type|defer|src|language]',
+                menubar: false,
+                statusbar: false,
+                skin: 'orchardlightgray',
+                inline: true
+            });
+
+        console.log('Finalized Editor');
+    });
+
+    $inlineedit.bind(inlineedit.events.onCancelling, function (event, shape, shapeEditor) {
+
+        var editorFieldId = $(shapeEditor).find(".tinymce").attr("id");
+        tinymce.get(editorFieldId).remove();
+
+
+    });
+
+    var editor = {
+
+    };
+
+    if (!window.orchard) {
+        window.orchard = {};
+    }
+
+    if (!window.orchard.inlineedit) {
+        window.orchard.inlineedit = {};
+    }
+
+    if (!window.orchard.inlineedit) {
+        window.orchard.inlineedit.ui = {};
+    }
+
+    window.orchard.inlineedit.defaulteditor = editor;
+})(jQuery, window, window.orchard.inlineedit.ui, window.tinymce);
